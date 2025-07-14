867961bc5f4dee7dba2c6bff80979bde8c1d2442
Shreyboiii <shreyash.hamal@gmail.com>
2025-07-14 09:52:00 -0700

    Removing demographics test folder

--- src/app/demographics-test/page.tsx
+++
@@ -1,5 +0,0 @@
-import InstagramDemographicsExample from '../dashboard/content-analytics/components/InstagramDemographicsExample';
-
-export default function DemographicsTestPage() {
-  return <InstagramDemographicsExample />;
-} 
